092af7e2a2e95322e92a99f57f8c6c187210dc8a
Ben Brookes <brookesb91@hotmail.com>
2022-05-05 20:55:07 +0100

    Update router_link.dart

--- lib/widgets/router_link.dart
+++ lib/widgets/router_link.dart
@@ -4,19 +4,25 @@ import 'package:flutter/widgets.dart';
 class RouterLink extends StatelessWidget {
   final String path;
   final Map<String, String> queryParameters;
+  final bool clear;
   final Function(BuildContext context, void Function() go) builder;
 
   const RouterLink(
     this.path, {
     Key? key,
     this.queryParameters = const {},
+    this.clear = false,
     required this.builder,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return builder(context, () {
-      context.router.navigate(path, queryParameters: queryParameters);
+      context.router.navigate(
+        path,
+        queryParameters: queryParameters,
+        clear: clear,
+      );
     });
   }
 }
